bc308a6c9f5d569522fcf0b66a8c8b4bd97e0459
Gianluca Fuoco <gianluca_1227@hotmail.com>
2022-03-29 20:04:37 -0400

    upated app to render the board

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Board from "./Board/Board";
 
 const App: React.FC = () => {
-  return <div className="app">Hello, World!</div>;
+  return (
+    <div className="app">
+      <h1>jChess</h1>
+      <Board />
+    </div>
+  );
 };
 
 export default App;
