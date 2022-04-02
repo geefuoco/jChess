60a6559f151e6fd79b4d469c91afe0d806f2937c
Gianluca Fuoco <gianluca_1227@hotmail.com>
2022-04-02 11:57:14 -0400

    Created the sliding piece base class

---
+++ src/chess/pieces/slidingPiece.ts
@@ -0,0 +1,40 @@
+import { Position } from "../interfaces/position";
+import { Move } from "../moves/move";
+import { Piece } from "./piece";
+
+export class SlidingPiece extends Piece {
+  generateMoveSet(): Move[] {
+    const that = this; //eslint-disable-line
+    type childPiece = {
+      constructor: childPiece;
+      MOVE_SET: number[][];
+    } & typeof that;
+    const moves: Move[] = [];
+    (this as childPiece).constructor.MOVE_SET.forEach((moveSet) => {
+      const newPosition = {
+        x: moveSet[0] + this.position.x,
+        y: moveSet[1] + this.position.y
+      };
+      this.getSlidingMoves(newPosition, moveSet, moves);
+    });
+    return moves;
+  }
+
+  private getSlidingMoves(
+    newPosition: Position,
+    moveSet: number[],
+    moves: Move[]
+  ) {
+    while (!this.outsideOfBoard(newPosition)) {
+      moves.push(new Move(newPosition));
+      if (this.hasPiece(newPosition)) break;
+      const rankOffset = moveSet[0] + newPosition.x;
+      const fileOffset = moveSet[1] + newPosition.y;
+      newPosition = { x: rankOffset, y: fileOffset };
+    }
+  }
+
+  private outsideOfBoard(position: Position): boolean {
+    return position.x < 0 || position.x > 7 || position.y < 0 || position.y > 7;
+  }
+}
