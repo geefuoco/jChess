428bae14cbfb5e33ea04c6da45b462854d59876f
Gianluca Fuoco <gianluca_1227@hotmail.com>
2022-04-23 14:55:07 -0400

    Created a Context for the Chessboard to be accessed easily Fixed generateBoard function. The column value is already a piece, no need to grab the piece from the board changed styling of site

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,33 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import "./Board.css";
 import Cell from "../Cell/Cell";
-import { ChessBoard, pieceMap } from "../../chess/board/chessboard";
-import { Fen } from "../../chess/fen/fen";
-
-const chessBoard = new ChessBoard();
-const fen = new Fen({
-  pieces: "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR",
-  activeColor: "w",
-  castling: "KQkq",
-  enPassent: "-",
-  halfMove: 0,
-  fullMove: 1,
-  board: chessBoard
-});
-fen.setBoardFromFen();
+import { pieceMap } from "../../chess/board/chessboard";
+import { ChessBoardContext } from "../..";
 
 const Board: React.FC = () => {
+  const chessBoard = useContext(ChessBoardContext);
   const [board, setBoard] = useState(chessBoard.getBoard());
 
   const generateBoard = (): JSX.Element[] => {
     const elements: JSX.Element[] = [];
     board.forEach((row, rowIdx) => {
       const cellRow: JSX.Element[] = [];
-      row.forEach((col, colIdx) => {
-        const square = chessBoard.getSquare({ x: rowIdx, y: colIdx });
+      row.forEach((piece, colIdx) => {
         cellRow.push(
           <Cell
-            piece={square ? pieceMap[square.getPieceCode()] : undefined}
+            piece={piece ? pieceMap[piece.getPieceCode()] : undefined}
             light={((colIdx + rowIdx) & 1) === 1}
             key={`${rowIdx}-${colIdx}`}
           />
--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,29 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./components/App";
+import { ChessBoard } from "./chess/board/chessboard";
+import { Fen } from "./chess/fen/fen";
+
+const chessBoard = new ChessBoard();
+const fen = new Fen({
+  pieces: "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR",
+  activeColor: "w",
+  castling: "KQkq",
+  enPassent: "-",
+  halfMove: 0,
+  fullMove: 1,
+  board: chessBoard
+});
+fen.setBoardFromFen();
+
+export const ChessBoardContext = React.createContext<ChessBoard>(chessBoard);
 
 const root = createRoot(document.getElementById("root") as HTMLElement);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <ChessBoardContext.Provider value={chessBoard}>
+      <App />
+    </ChessBoardContext.Provider>
   </React.StrictMode>
 );
