596888e32256a73d7ba8e06a3ddfa58c5d43afdc
Gianluca Fuoco <gianluca_1227@hotmail.com>
2022-06-11 12:27:08 -0400

    removed unused imports

--- src/components/Moves/Moves.tsx
+++ src/components/Moves/Moves.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { ChessBoard } from "../../chess/board/chessboard";
 import { Fen } from "../../chess/fen/fen";
 import { Piece } from "../../chess/pieces/piece";
